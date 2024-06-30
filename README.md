ea59e5bef51c4544a5c611df19faeea4edbff00a
Asmaa01-hub <husseinasmahan89@gmail.com>
2024-06-30 22:31:23 +0300

    completed

---
+++ README.md
@@ -0,0 +1,14 @@
+Week 1 Code Challenge
+AUTHOR
+ASMAA HUSSEIN
+
+DESCRIPTION
+Student Grade Generator
+This program assigns grades to students depending on the marks that have been input by the user when prompted.
+
+Speed Detector
+The program checks the speed of car and outputs consequences depending on the number of demerit points assigned when the speed is above 70km/h.And if the user has more than 12 demerit points assigned the license is suspended .
+
+Net Salary Calculator
+Program calculates the net salary of an individual when given the basic salary and the benefits with live(current tax rates).
+
---
+++ speedDetector.js
@@ -0,0 +1,27 @@
+
+
+// Prompt the user for input
+
+
+function calculateDemeritPoints() {
+    const speed = prompt(" carspeed:");
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+  
+    if (speed <= speedLimit) {
+        console.log("OK");
+        window.alert('OK')
+      //   return; 
+    }else {
+    const excessSpeed = speed - speedLimit;
+    const demeritPoints = Math.floor(excessSpeed / kmPerDemeritPoint);
+  
+    if (demeritPoints <= 12) {
+      window.alert(`You have ${demeritPoints} demerit point(s).`);
+    } else {
+      window.alert('license suspended')
+    }
+  }
+  }
+  calculateDemeritPoints();
+  
